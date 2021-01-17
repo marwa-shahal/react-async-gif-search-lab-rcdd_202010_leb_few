37a8904c84ee2448bb891e0943512efc031d877a
Marwa Sh <marwa.al.shahal@gmail.com>
2021-01-17 20:15:01 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -4,17 +4,16 @@ export default class GifList extends Component{
   render(){
     return(
       <ul>
-    {this.props.gifs.map(gif => 
-    <li>
-    <div>
-    <img src={gif.url} alt="gif"/>
-    </div>
-    </li>)}
+          {this.props.gifs.map(gif => 
+                     <li>
+                         <div>
+                            <img src={gif.url} alt="gif"/>
+                         </div>
+                       </li>)}
     </ul>
     )
   }
   
 }
-
 
 
